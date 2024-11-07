0e73006b5d69a629fb8d3894279d057f3c798546
levbitz <maxinosamx@gmail.com>
2024-11-07 23:46:37 +0300

    home page udate"

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-    <h1>Home Page</h1>
+    <h1>Home Page  Brain 123334445</h1>
     <h1>Only Accessed by logged in Users</h1>
     <button className='btn'>Log out </button>
     </div>
